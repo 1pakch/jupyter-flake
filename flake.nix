d5b56822f5197979d069bbca6f0128b16d9dbe78
Ilya Kolpakov <ikolpakov@health2030.ch>
2024-03-21 15:23:36 +0100

    add jupyter-lab as default app

--- flake.nix
+++ flake.nix
@@ -8,5 +8,9 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.jupyter = nixpkgs.legacyPackages.x86_64-linux.jupyter;
     packages.x86_64-linux.default = self.packages.x86_64-linux.jupyter;
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.jupyter}/bin/jupyter-lab";
+    };
   };
 }
