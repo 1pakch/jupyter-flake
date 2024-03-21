c7ccfa74df88b9d45270646dff441e667fb69210
Ilya Kolpakov <ikolpakov@health2030.ch>
2024-03-21 21:43:49 +0100

    add another python kernel with numpy

--- flake.nix
+++ flake.nix
@@ -5,11 +5,31 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pythonEnv = pkgs.python3.withPackages (ps: [ps.ipykernel ps.numpy]);
+    kernelspecs = {
+      pythonenv = let
+        env = pythonEnv;
+      in {
+        displayName = "Python 3 with numpy";
+        argv = [
+          env.interpreter
+          "-m"
+          "ipykernel_launcher"
+          "-f"
+          "{connection_file}"
+        ];
+        language = "python";
+        logo32 = "${env}/${env.sitePackages}/ipykernel/resources/logo-32x32.png";
+        logo64 = "${env}/${env.sitePackages}/ipykernel/resources/logo-64x64.png";
+      };
+    };
+  in {
 
     # Packages
 
-    packages.x86_64-linux.jupyter = nixpkgs.legacyPackages.x86_64-linux.jupyter;
+    packages.x86_64-linux.jupyter = pkgs.jupyter.override { definitions = kernelspecs; };
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.jupyter;
 
