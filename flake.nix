b82b6240c241697db8f9a0232be622659e36831f
Ilya Kolpakov <ikolpakov@health2030.ch>
2024-03-21 14:48:14 +0100

    add jupyter package as default

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "A flake with Jupyter package";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.jupyter = nixpkgs.legacyPackages.x86_64-linux.jupyter;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.jupyter;
   };
 }
