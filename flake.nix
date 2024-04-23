4e5cda7e92d782418a0158bec4e28fe7cd941035
Ilya Kolpakov <ikolpakov@health2030.ch>
2024-04-23 18:34:39 +0200

    support customizing extraLibs of jupyter server package

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: let
+
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
     ks-utils = import ./kernelspec-utils.nix;
     kernelspecs = {
@@ -29,24 +30,43 @@
 
     };
 
+    kernelspecs-folder = ks-utils.materialize {
+      lib = pkgs.lib;
+      stdenv = pkgs.stdenv;
+      definitions = kernelspecs; 
+    };
+
+    # Taken from nixpkgs - we need it here in order to be able to customize `extraLibs`
+    jupyter-env = (
+      pkgs.python3.buildEnv.override {
+        extraLibs = [
+          pkgs.python3.pkgs.notebook
+          pkgs.python3.pkgs.jupytext
+        ];
+        makeWrapperArgs = ["--set JUPYTER_PATH ${kernelspecs-folder}"];
+      }
+    ).overrideAttrs(oldAttrs: {
+      meta = oldAttrs.meta // { mainProgram = "jupyter-lab"; };
+    });
+
   in {
 
     # Packages
 
-    packages.x86_64-linux.jupyter = pkgs.jupyter.override { definitions = kernelspecs; };
+    packages.x86_64-linux.jupyter-env = jupyter-env;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.jupyter;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.jupyter-env;
 
     # Applications
 
     apps.x86_64-linux.jupyter-lab = {
       type = "app";
-      program = "${self.packages.x86_64-linux.jupyter}/bin/jupyter-lab";
+      program = "${self.packages.x86_64-linux.jupyter-env}/bin/jupyter-lab";
     };
 
     apps.x86_64-linux.jupyter-notebook = {
       type = "app";
-      program = "${self.packages.x86_64-linux.jupyter}/bin/jupyter-notebook";
+      program = "${self.packages.x86_64-linux.jupyter-env}/bin/jupyter-notebook";
     };
 
     apps.x86_64-linux.default = self.apps.x86_64-linux.jupyter-lab;
--- kernelspec-utils.nix
+++ kernelspec-utils.nix
@@ -1,5 +1,6 @@
 rec {
 
+  # A utility function to generate kernel names visible in Jupyter UI
   makeDisplayName = {
     interpreter-name,
     suffix ? ""
@@ -8,6 +9,8 @@ rec {
       then interpreter-name + " " + suffix
       else interpreter-name;
 
+  # Generates a Jupyter kernel definition from a Python environment
+  # (e.g. created using `python3.withPackages`)
   fromPythonEnv = { env, name ? null, suffix ? ""}: {
     displayName = makeDisplayName {
       interpreter-name = if name == null then "Python ${env.pythonVersion}" else name;
@@ -25,6 +28,8 @@ rec {
     logo64 = "${env}/${env.sitePackages}/ipykernel/resources/logo-64x64.png";
   };
 
+  # Generates a Jupyter kernel definition from an R envirionment
+  # (e.g. created using `rWrapper`)
   fromREnv = { env, name ? null, suffix ? ""} : {
     displayName = makeDisplayName {
       interpreter-name = if name == null then "R" else name;
@@ -43,4 +48,50 @@ rec {
     logo64 = null;
   };
 
+  # Writes out a folder with kernel specs that can be used by Jupyter
+  # (takes an attrset of kernel definitions as input)
+  # Taken from nixpkgs.
+  materialize = {
+    lib,
+    stdenv,
+    definitions
+  }: with lib; stdenv.mkDerivation {
+
+    name = "jupyter-kernels";
+
+    src = "/dev/null";
+
+    unpackCmd = "mkdir jupyter_kernels";
+
+    installPhase =  ''
+      mkdir kernels
+
+      ${concatStringsSep "\n" (mapAttrsToList (kernelName: unfilteredKernel:
+        let
+          allowedKernelKeys = ["argv" "displayName" "language" "interruptMode" "env" "metadata" "logo32" "logo64" "extraPaths"];
+          kernel = filterAttrs (n: v: (any (x: x == n) allowedKernelKeys)) unfilteredKernel;
+          config = builtins.toJSON (
+            kernel
+            // {display_name = if (kernel.displayName != "") then kernel.displayName else kernelName;}
+            // (optionalAttrs (kernel ? interruptMode) { interrupt_mode = kernel.interruptMode; })
+          );
+          extraPaths = kernel.extraPaths or {}
+            // lib.optionalAttrs (kernel.logo32 != null) { "logo-32x32.png" = kernel.logo32; }
+            // lib.optionalAttrs (kernel.logo64 != null) { "logo-64x64.png" = kernel.logo64; }
+          ;
+          linkExtraPaths = lib.mapAttrsToList (name: value: "ln -s ${value} 'kernels/${kernelName}/${name}';") extraPaths;
+        in ''
+          mkdir 'kernels/${kernelName}';
+          echo '${config}' > 'kernels/${kernelName}/kernel.json';
+          ${lib.concatStringsSep "\n" linkExtraPaths}
+        '') definitions)}
+
+      mkdir $out
+      cp -r kernels $out
+    '';
+
+    meta = {
+      description = "Writes out kernel definitions so that they can be read by Jupyter";
+    };
+  };
 }
